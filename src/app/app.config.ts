dc075dc9a493265754fecf62667c156f772067bf
Martin <martindzeble@gmail.com>
2025-08-08 11:52:47 +0000

    Updated Angular Project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withRouterConfig({paramsInheritanceStrategy: 'always'}))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withRouterConfig({paramsInheritanceStrategy: 'always'})), provideHttpClient()]
 };
