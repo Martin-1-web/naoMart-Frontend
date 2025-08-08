8dffe22504dae82cf0806f6bb65fa982ed40feaa
Martin <martindzeble@gmail.com>
2025-08-08 10:17:28 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+
+import { HeaderComponent } from "../naoMart/header/header.component";
+import { WelcomeComponent } from "../naoMart/welcome/welcome.component";
+import { NavbarComponent } from "../naoMart/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../naoMart/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, WelcomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../naoMart/home/home.component';
+import { AboutComponent } from '../naoMart/about/about.component';
+import { CoursesComponent } from '../naoMart/courses/courses.component';
+import { ContactComponent } from '../naoMart/contact/contact.component';
+import { WelcomeComponent } from '../naoMart/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: WelcomeComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
+];
