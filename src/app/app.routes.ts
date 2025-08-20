e39e1a799beab25c6ef09088dce66915cf334964
Martin <martindzeble@gmail.com>
2025-08-20 01:51:58 +0000

    Fixed Navigation Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from '../demo/welcome/welcome.component';
 import { CoursesComponent } from '../demo/courses/courses.component';
+import { AboutComponent } from '../demo/about/about.component';
+import { ContactComponent } from '../demo/contact/contact.component';
 
 export const routes: Routes = [
     {path: 'home', component: WelcomeComponent},
-    {path: 'courses', component: CoursesComponent}
+    {path: 'courses', component: CoursesComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
 ];
